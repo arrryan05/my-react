bfe5623198e70c983f7d24359f5ffb653f422a28
sneh-soni <ssneh20062003@gmail.com>
2024-04-17 18:21:56 +0530

    Hero text correction

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import CustomButton from "./CustomButton";
 const Hero = () => {
   return (
     <div
-      className="w-full h-screen flex justify-center items-center flex-col gap-4"
+      className="w-full h-screen flex justify-center items-center flex-col gap-4 pt-10"
       style={{
         backgroundImage:
           'url("https://chooseacacia.com/wp-content/uploads/2024/01/AdobeStock_2516269-scaled.jpg")',
@@ -12,10 +12,10 @@ const Hero = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="px-6 text-5xl md:text-6xl md:w-1/3 font-bold text-white">
+      <div className="px-6 text-center text-5xl md:text-6xl font-bold text-white">
         Spark Ideas Into Action
       </div>
-      <div className="text-white md:w-3/4 px-6">
+      <div className="text-white px-16 text-center">
         Acacia empowers business and technology decisions that control costs,
         mitigate risks and deliver exceptional business value with breakthrough
         technologies like AI.
